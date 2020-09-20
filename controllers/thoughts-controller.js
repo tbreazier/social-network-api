ef731aff2da094b071579761c20f79f6980b6a38
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-09-20 14:59:04 -0500

    started controllers

---
+++ controllers/thoughts-controller.js
@@ -0,0 +1,13 @@
+//Get all thoughts
+
+//Get single though by ID
+
+//Create new thought (POST)
+
+//Update a thought by ID (PUT)
+
+//Delete a thought by ID
+
+//Create a reaction to a thought
+
+//Delete a reaction by ID
---
+++ controllers/user-controller.js
@@ -0,0 +1,13 @@
+//Get All Users
+
+//Get Single user by ID
+
+//Create new user (POST)
+
+//Update User By ID (PUT)
+
+//Delete User by ID
+
+//Add new friend to user's friend list (POST)
+
+//Remove Friend from user's friend list (DELETE)
