4c249e8b6074fd74e9dec1bd3252339a8a5b71c4
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-09-20 21:07:19 -0500

    working on thought controller

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -1,13 +1,80 @@
-//Get all thoughts
+const { User, Thought } = require('../models');
 
-//Get single though by ID
+const thoughtController = {
+    //Get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .populate({
+                path: 'reactions',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                  res.json({ message: 'No thought found with this id!' });
+                  return;
+                }
+                res.json(dbThoughtData);
+              })
+              .catch(err => {
+                res.json(err);
+              });
+    },
+    //Get single though by ID
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .populate({
+                path: 'reactions',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+    //Create new thought (POST)
+    createThought({ params, body}, res) {
+        console.log(body);
+        Thought.create(body)
+            .then(({_id}) => {
+                return User.findOneAndUpdate(
+                    {_id: params.id},
+                    {$push: {thoughts: _id}},
+                    {new: true}
+                );
+                })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No user found with this ID!'});
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });    
+    },
 
-//Create new thought (POST)
+    //Update a thought by ID (PUT)
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
+          .then(dbThoughtData => {
+            if (!dbThoughtData) {
+              res.json({ message: 'No thought found with this id!' });
+              return;
+            }
+            res.json(dbThoughtData);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+    },
+    //Delete a thought by ID
 
-//Update a thought by ID (PUT)
+    //Create a reaction to a thought
 
-//Delete a thought by ID
-
-//Create a reaction to a thought
-
-//Delete a reaction by ID
+    //Delete a reaction by ID 
+}
